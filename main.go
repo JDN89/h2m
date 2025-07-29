a151a52860b79ccea65f9d19f4a85e4fc7085e6c
Jan DeNiels <jan.deniels@axians.com>
2025-07-29 21:36:44 +0200

    fetch and print contents of blog

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello h2m!")
+	res, err := http.Get("https://journal.stuffwithstuff.com/2015/02/01/what-color-is-your-function/")
+	if err != nil {
+		fmt.Println("Error fetching url:", err)
+		return
+	}
+	defer res.Body.Close()
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println("Error reading contents of response body")
+	}
+	fmt.Println("Fetched HTML:")
+	fmt.Println(string(body))
 }
