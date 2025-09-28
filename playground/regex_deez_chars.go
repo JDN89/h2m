5163ea6b519f29d063c7b4ab505367e98a0c93ae
JDN89 <jan.de.niels@gmail.com>
2025-09-28 21:48:42 +0200

    playing around with regex, Parsed the regex attributes. Implement in Lexer next

--- playground/regex_deez_chars.go
+++ playground/regex_deez_chars.go
@@ -1,25 +1,30 @@
 package playground
 
 import (
-	"fmt"
 	"regexp"
 )
 
 func ParseHref(input []byte) map[string]string {
 
-	attributes := make(map[string]string)
+	hrefRegexPatterns := map[string]*regexp.Regexp{
 
-	var hrefRe = regexp.MustCompile(`href="([^"]+)"`)
-
-	// FindSubmatch returns the whole match and then the capture groups if any are found
-	href := hrefRe.FindSubmatch(input)
-
-	if len(href) > 1 {
-		attributes["href"] = string(href[1])
+		"href":  regexp.MustCompile(`href="([^"]+)"`),
+		"rel":   regexp.MustCompile(`rel="([^"]+)"`),
+		"title": regexp.MustCompile(`title="([^"]+)"`),
 	}
 
-	fmt.Println(attributes["href"])
+	hrefAttributes := make(map[string]string)
 
-	return attributes
+	// FindSubmatch returns the whole match and then the capture groups if any are found
+
+	for key, regex := range hrefRegexPatterns {
+		attributeMatch := regex.FindSubmatch(input)
+		if len(attributeMatch) > 1 {
+			hrefAttributes[key] = string(attributeMatch[1])
+		}
+
+	}
+
+	return hrefAttributes
 
 }
--- playground/regex_deez_chars_test.go
+++ playground/regex_deez_chars_test.go
@@ -9,8 +9,20 @@ func TestRegex(t *testing.T) {
 
 	result := ParseHref([]byte(input))
 
-	if result["href"] != "/2015/02/01/what-color-is-your-function/" {
-		t.Errorf("expected href to be extracted, got %q", result["href"])
+	expected := map[string]string{
+		"href":  "/2015/02/01/what-color-is-your-function/",
+		"rel":   "bookmark",
+		"title": "Permanent Link to What Color is Your Function?",
 	}
-}
 
+	if len(result) != len(expected) {
+		t.Fatalf("number of elements in attributes map doesn't match. We expected %d , but got %d", len(expected), len(result))
+	}
+
+	for attr, content := range expected {
+		if result[attr] != content {
+			t.Fatalf("Expected %s, but got %s ", content, result[attr])
+		}
+	}
+
+}
